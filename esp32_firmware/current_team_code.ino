0628a9cb7768ac4e5c99571cb372f223bfd7b82b
Omnia Bogdady <omniaboghdady2004@gmail.com>
2026-05-19 06:05:31 +0300

    fix

--- esp32_firmware/current_team_code.ino
+++ esp32_firmware/current_team_code.ino
@@ -7,25 +7,33 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <geometry_msgs/msg/quaternion.h>
+#include <std_msgs/msg/int32.h> // تضمين مكتبة الأرقام للإنكودر
 
 // --- إعدادات شبكة الـ Wi-Fi و micro-ROS Agent ---
-const char* ssid = "YOUR_WIFI_NAME";        // اسم شبكة الواي فاي
-const char* password = "YOUR_WIFI_PASSWORD"; // باسورد الواي فاي
-IPAddress agent_ip(192, 168, 1, 100);       // الـ IP بتاع اللاب توب بتاعك
-const uint16_t agent_port = 8888;           // البورت الافتراضي لـ micro-ROS Agent
+const char* ssid = "wafaa hammad";        
+const char* password = "12345678";        
+const char* agent_ip = "192.168.137.88";  
+const uint16_t agent_port = 8888;         
 
 // --- كائنات micro-ROS ---
 rcl_subscription_t subscriber;
 geometry_msgs__msg__Quaternion msg;
+
+// Publishers للإنكودر
+rcl_publisher_t pub_enc_l;
+rcl_publisher_t pub_enc_r;
+std_msgs__msg__Int32 msg_enc_l;
+std_msgs__msg__Int32 msg_enc_r;
+
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){/* خطأ في الاتصال */} }
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){/* خطأ */} }
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){/* تحذير */} }
 
-// --- PIN DEFINITIONS ---
+// --- PIN DEFINITIONS (IR & Motors) ---
 #define PIN_IR_L2 36
 #define PIN_IR_L1 39
 #define PIN_IR_MID 34
@@ -39,6 +47,13 @@ rcl_node_t node;
 #define PIN_DATA 23    
 #define PIN_EN 22      
 #define PIN_LED 13
+
+// --- PIN DEFINITIONS (Encoders) ---
+// اختاري الدبابيس المناسبة لتوصيل الإنكودر
+#define PIN_ENC_L_A 16 
+#define PIN_ENC_L_B 17
+#define PIN_ENC_R_A 4
+#define PIN_ENC_R_B 5
 
 const int ir_pins[] = { PIN_IR_L2, PIN_IR_L1, PIN_IR_MID, PIN_IR_R1, PIN_IR_R2 };
 const int pwm_freq = 5000;
@@ -55,7 +70,31 @@ float prev_error = 0.0;
 float integral = 0.0;   
 float left_trim_factor = 1.145; 
 
-// دالة تحديث قيم الـ PID اللي بيستدعيها الـ Agent
+unsigned long last_ros_time = 0; 
+
+// --- متغيرات ودوال الإنكودر (Interrupts) ---
+volatile int32_t left_ticks = 0;
+volatile int32_t right_ticks = 0;
+
+// دالة عد نبضات العجلة اليسرى
+void IRAM_ATTR left_enc_isr() {
+  if (digitalRead(PIN_ENC_L_B) == HIGH) {
+    left_ticks++;
+  } else {
+    left_ticks--;
+  }
+}
+
+// دالة عد نبضات العجلة اليمنى
+void IRAM_ATTR right_enc_isr() {
+  if (digitalRead(PIN_ENC_R_B) == HIGH) {
+    right_ticks++;
+  } else {
+    right_ticks--;
+  }
+}
+
+// دالة تحديث قيم الـ PID من الـ ROS
 void tuneCallback(const void * msgin) {
   const geometry_msgs__msg__Quaternion * received_msg = (const geometry_msgs__msg__Quaternion *)msgin;
   kp = received_msg->x;           
@@ -64,8 +103,7 @@ void tuneCallback(const void * msgin) {
   base_speed = received_msg->w;   
 }
 
-// --- متغيرات التحكم في الروبوت ---
-enum State { FOLLOWING, FINISHED }; // تم إزالة حالات التوقف والدوران
+enum State { FOLLOWING, FINISHED }; 
 State robotState = FOLLOWING;
 uint8_t latch_state = 0;
 int lap_counter = 0;           
@@ -119,34 +157,63 @@ void setup() {
   ledcAttach(PIN_M2_PWM, pwm_freq, pwm_res);
   digitalWrite(PIN_LED, LOW); 
 
-  // --- إعداد اتصال micro-ROS عبر الواي فاي ---
-  set_microros_wifi_transports((char*)ssid, (char*)password, agent_ip, agent_port);
+  // --- إعداد دبابيس الإنكودر ---
+  pinMode(PIN_ENC_L_A, INPUT_PULLUP);
+  pinMode(PIN_ENC_L_B, INPUT_PULLUP);
+  pinMode(PIN_ENC_R_A, INPUT_PULLUP);
+  pinMode(PIN_ENC_R_B, INPUT_PULLUP);
+  
+  // تفعيل المقاطعات (Interrupts) للعد التلقائي للنبضات
+  attachInterrupt(digitalPinToInterrupt(PIN_ENC_L_A), left_enc_isr, RISING);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENC_R_A), right_enc_isr, RISING);
 
-  delay(2000); // ننتظر قليلاً للاتصال بالشبكة
+  // --- إعداد اتصال micro-ROS عبر الواي فاي ---
+  set_microros_wifi_transports((char*)ssid, (char*)password, (char*)agent_ip, agent_port);
+  delay(2000); 
 
   allocator = rcl_get_default_allocator();
-
-  // تهيئة الـ Support والـ Node
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-  RCCHECK(rclc_node_init_default(&node, "pid_tuner_node", "", &support));
+  RCCHECK(rclc_node_init_default(&node, "robot_core_node", "", &support));
 
-  // إنشاء الـ Subscriber
+  // إنشاء الـ Subscriber للـ PID
   RCCHECK(rclc_subscription_init_default(
-    &subscriber,
-    &node,
+    &subscriber, &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Quaternion),
     "tune_pid"));
 
-  // إعداد الـ Executor (المسؤول عن تنفيذ الـ Callbacks)
+  // --- إنشاء الـ Publishers للإنكودر ---
+  RCCHECK(rclc_publisher_init_default(
+    &pub_enc_l, &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "/raw/encoder_l"));
+
+  RCCHECK(rclc_publisher_init_default(
+    &pub_enc_r, &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "/raw/encoder_r"));
+
+  // إعداد الـ Executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &tuneCallback, ON_NEW_DATA));
 }
 
 void loop() {
-  // استقبال الأوامر من micro-ROS Agent (يتم تنفيذها إذا كان هناك بيانات جديدة)
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
+  // 1. --- جزء الـ ROS (يشتغل كل 100 مللي ثانية فقط) ---
+  if (millis() - last_ros_time > 100) {
+    // تحديث قيم الإنكودر في رسائل الـ ROS
+    msg_enc_l.data = left_ticks;
+    msg_enc_r.data = right_ticks;
 
-  // قراءة الحساسات
+    // إرسال البيانات لكود البايثون
+    RCSOFTCHECK(rcl_publish(&pub_enc_l, &msg_enc_l, NULL));
+    RCSOFTCHECK(rcl_publish(&pub_enc_r, &msg_enc_r, NULL));
+
+    // استقبال الأوامر الجديدة
+    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(0)));
+    last_ros_time = millis();
+  }
+
+  // 2. --- جزء التحكم السريع (Line Follower) ---
   float sensor_values[5];
   float total_on_line = 0;
   for (int i = 0; i < 5; i++) {
@@ -160,10 +227,10 @@ void loop() {
     case FOLLOWING:
       if (total_on_line >= 4) {
         if (on_finish_line == false) {
-          lap_counter++;               // زود عداد اللفات
-          on_finish_line = true;       // تأمين العداد لتسجيل لفة واحدة فقط في كل مرور
+          lap_counter++;               
+          on_finish_line = true;       
           if (lap_counter != 1){
-            digitalWrite(PIN_LED, HIGH); // نور الليد أول ما يلمس الخط
+            digitalWrite(PIN_LED, HIGH); 
           }
         }
       } else {
@@ -179,8 +246,6 @@ void loop() {
       } 
       else {
         float error;
-
-        // التعديل الجديد: إذا فقد الخط، نستخدم آخر خطأ مسجل بدلاً من التوقف
         if (total_on_line == 0) {
           error = prev_error; 
         } else {
@@ -216,6 +281,6 @@ void loop() {
       set_motor_speed(2, 0);
       break;
   }
-  // قللنا الـ Delay لأن الـ executor_spin_some بيعمل استهلاك للوقت شوية
-  delay(5); 
+  
+  delay(1); 
 }
